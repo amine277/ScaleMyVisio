3c619b421b4db6b6ae3b695e62af7686988c7583
Mohammed Touda Lachiri <mtoudalachi@enseirb-matmeca.fr>
2021-03-09 14:09:00 +0100

    Modifs

--- docker/server.js
+++ docker/server.js
@@ -1,6 +1,6 @@
 
 
-var http = require('http');
+/*var http = require('http');
 var fs = require('fs');
 
 // Constants
@@ -31,4 +31,37 @@ var server = http.createServer(function (request, response) {
 server.listen(PORT);
 
 console.log(`Nodejs Running on http://localhost:3000`);
-console.log(`PHPMyAdmin Running on http://localhost:3001`);
+console.log(`PHPMyAdmin Running on http://localhost:3001`);*/
+
+
+const express = require("express")
+const app = express()
+
+
+
+
+app.use(express.static(__dirname + '/src'));
+
+/*app.put('/products/:number/:name/:prix', (req, res) => {
+
+    products[req.params.number]={name:req.params.name, prix:req.params.prix};
+    res.json(products);
+});
+app.post("/products", function(req, res) {
+    products.push({"name": req.query.name , "prix":req.query.prix });
+    res.status(201).json(product);
+  });*/
+
+/*app.post('/products/', (req, res) => {
+
+    products.push({"name": req.query.name , "prix":req.query.prix });
+    res.send(products);
+});*/
+
+/*app.get('/:file', (req, res) => {/*
+    res.send(req.params.file);
+});*/
+
+app.listen(8080, () => {
+  console.log("En attente de requêtes...")
+})
